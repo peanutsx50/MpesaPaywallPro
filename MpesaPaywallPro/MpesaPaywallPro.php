36d2eed2ec6a9f6bab82ca549d95b5240ffd73e8
peanutsx50 <murimifestus09@gmail.com>
2026-01-17 18:05:00 +0300

    Add initial plugin structure and functionality for MpesaPaywallPro
    - Created main plugin file MpesaPaywallPro.php with plugin metadata and activation/deactivation hooks.
    - Added README.txt for plugin description, installation instructions, and changelog.
    - Implemented admin functionality in class-plugin-name-admin.php, including styles and scripts enqueueing.
    - Created CSS and JS files for admin interface.
    - Established base classes for activation, deactivation, internationalization, and loader functionality.
    - Developed public-facing functionality in class-plugin-name-public.php with corresponding styles and scripts.
    - Included partials for both admin and public views.
    - Added uninstall.php for handling plugin uninstallation.
    - Integrated Composer for dependency management with composer.json.
    - Created language files for internationalization support.

---
+++ MpesaPaywallPro/MpesaPaywallPro.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin URI:        http://example.com/plugin-name-uri/
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Your Name or Your Company
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+added simple composer
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (! defined('WPINC')) {
+	die;
+}
+
+// Autoload dependencies using Composer
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ * 
+ */
+
+// setting up plugin constants
+define('PLUGIN_NAME_VERSION', '1.0.0');
+define('PLUGIN_NAME_URL', plugin_dir_url(__FILE__));
+define('PLUGIN_NAME_PATH', plugin_dir_path(__FILE__));
+
+// namespace imports
+use PluginName\base\Plugin_Name_Activator;
+use PluginName\base\Plugin_Name_Deactivator;
+use PluginName\base\Plugin_Name;
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+ */
+function activate_plugin_name()
+{
+	Plugin_Name_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_plugin_name()
+{
+	Plugin_Name_Deactivator::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_plugin_name');
+register_deactivation_hook(__FILE__, 'deactivate_plugin_name');
+
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+
+function run_plugin_name()
+{
+
+	$plugin = new Plugin_Name();
+	$plugin->run();
+}
+run_plugin_name();
