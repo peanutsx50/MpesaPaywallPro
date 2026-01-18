4a6b426b9d6ec0ad738dbda1432f3422c9e4af46
peanutsx50 <murimifestus09@gmail.com>
2026-01-18 15:39:07 +0300

    Add access control and paywall settings partials: implement content rendering for admin settings tabs

---
+++ MpesaPaywallPro/admin/partials/access-control.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Access Control Tab Content
+ *
+ * This file is responsible for rendering the content of the "Access Control" tab
+ * within the Mpesa Paywall Pro plugin's admin settings page.
+ * It provides options for administrators to manage access control settings
+ * related to premium content and user permissions.
+ * 
+ * @package MpesaPaywallPro\Admin\Partials
+ * @since 1.0.0
+ * 
+ * @link       https://festuswp.gumroad.com/l/MpesaPaywallPro
+ *  
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+
+<div>
+    <h1><?php esc_html_e('Access Control Settings', 'mpesapaywallpro'); ?></h1>
+</div>
---
+++ MpesaPaywallPro/admin/partials/mpesa-setup.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Paywall Settings Tab Content
+ * 
+ * This file is responsible for rendering the content of the "Paywall Settings" tab
+ * within the Mpesa Paywall Pro plugin's admin settings page.
+ * It provides options for administrators to configure paywall settings
+ * related to premium content access and payment requirements.
+ * 
+ * @package MpesaPaywallPro\Admin\Partials
+ * @since 1.0.0
+ * 
+ * @link       https://festuswp.gumroad.com/l/MpesaPaywallPro
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+
+<div>
+    <h1><?php esc_html_e('Paywall Settings', 'mpesapaywallpro'); ?></h1>
+</div>
