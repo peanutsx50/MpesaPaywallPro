732238ebd52064e0da03b3e85535ab9dba7bae1e
peanutsx50 <murimifestus09@gmail.com>
2026-01-20 14:12:17 +0300

    Refactor notice display logic: change notice visibility to flex when not closed and improve close button functionality

--- MpesaPaywallPro/admin/js/admin-settings.js
+++ MpesaPaywallPro/admin/js/admin-settings.js
@@ -1,23 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Close button functionality for the notice
-    const closeBtn = document.querySelector('.mpesapaywallpro-notice-close');
-    const notice = document.querySelector('.mpesapaywallpro-notice');
-    //check if cookie is set to hide notice
-    const closedNotice = get_cookie("mpesapaywallpro_notice_closed");
-    if (closedNotice === "true" && notice) {
-        notice.style.display = 'none';
-    }
-    
-    //add event listener to close button
-    if (closeBtn && notice) {
-        closeBtn.addEventListener('click', function () {
-            notice.style.display = 'none';
-            document.cookie = "mpesapaywallpro_notice_closed=true; path=/; max-age=" + 60 * 60 * 24 * 30; // 30 days
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  // Check if cookie is set to hide notice
+  const closedNotice = get_cookie("mpesapaywallpro_notice_closed");
+  const notice = document.querySelector(".mpesapaywallpro-notice");
+  
+  if (!closedNotice && notice) {
+    notice.style.display = "flex";
+  }
+
+  // Close button functionality for the notice
+  const closeBtn = document.querySelector(".mpesapaywallpro-notice-close");
+
+  // Add event listener to close button
+  if (closeBtn && notice) {
+    closeBtn.addEventListener("click", function () {
+      notice.style.display = "none";
+      document.cookie =
+        "mpesapaywallpro_notice_closed=true; path=/; max-age=" +
+        60 * 60 * 24 * 30; // 30 days
+    });
+  }
 });
 
-// function to get cookie
+// Function to get cookie
 function get_cookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
