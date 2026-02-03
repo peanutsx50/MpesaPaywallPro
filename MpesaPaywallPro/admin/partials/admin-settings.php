abd6c051961cd366f229bb508614057479599ea1
peanutsx50 <murimifestus09@gmail.com>
2026-02-03 22:53:16 +0300

    Use esc_url() for external links in admin settings and M-Pesa setup templates

--- MpesaPaywallPro/admin/partials/admin-settings.php
+++ MpesaPaywallPro/admin/partials/admin-settings.php
@@ -53,13 +53,13 @@ $current_tab = isset($_GET['tab']) && array_key_exists($_GET['tab'], $tabs) ? $_
             </div>
             <div class="mpesapaywallpro-banner-right">
                 <div class="mpesapaywallpro-banner-actions">
-                    <a href="https://surgetech.co.ke/contact" target="_blank" class="button button-primary">
+                    <a href="<?php echo esc_url('https://surgetech.co.ke/contact'); ?>" target="_blank" class="button button-primary">
                         <div class="mpesapaywallpro-features-button">
                             <span class="dashicons dashicons-external"></span>
                             <?php esc_html_e('Contact us for custom plugin', 'mpesapaywallpro'); ?>
                         </div>
                     </a>
-                    <a href="https://surgetech.co.ke/mpesapaywallpro" target="_blank" class="button">
+                    <a href="<?php echo esc_url('https://surgetech.co.ke/mpesapaywallpro'); ?>" target="_blank" class="button">
                         <div class="mpesapaywallpro-features-button">
                             <span class="dashicons dashicons-book"></span>
                             <?php esc_html_e('Read how to use this plugin', 'mpesapaywallpro'); ?>
