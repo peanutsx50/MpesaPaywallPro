0883975d7cc6dd0aed0f3346756aa8c9ba3e30c9
peanutsx50 <murimifestus09@gmail.com>
2026-02-15 13:11:25 +0300

    fix: change action hook from 'plugins_loaded' to 'init' for loading plugin textdomain

--- mpesapaywallpro/includes/base/MpesaPaywallPro.php
+++ mpesapaywallpro/includes/base/MpesaPaywallPro.php
@@ -127,7 +127,7 @@ class MpesaPaywallPro
 
 		$plugin_i18n = new MpesaPaywallProI18n();
 
-		$this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
+		$this->loader->add_action('init', $plugin_i18n, 'load_plugin_textdomain');
 	}
 
 	/**
