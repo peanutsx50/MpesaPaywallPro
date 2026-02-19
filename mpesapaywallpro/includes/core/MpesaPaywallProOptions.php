798360d6bccd5a1fa8087a2aaf3d0c12f278545c
peanutsx50 <murimifestus09@gmail.com>
2026-02-19 11:16:05 +0300

    feat: add MpesaPaywallProOptions class for managing plugin options

---
+++ mpesapaywallpro/includes/core/MpesaPaywallProOptions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Singleton class to manage the plugin options.
+ * 
+ * This class provides methods to get and set the plugin options, as well as to initialize default options on plugin activation.
+ * It ensures that there is only one instance of the options manager throughout the plugin, and provides a centralized way to access and modify the plugin settings.
+ * The options are stored in the WordPress database using the `get_option` and `update_option` functions, and are defined as an associative array with default values.
+ * 
+ * 
+ * @package MpesaPaywallPro
+ * 
+ * @since 1.0.0
+ * @author Surgetech
+ * 
+ */
+
+namespace MpesaPaywallPro\Core;
+
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+
+class MpesaPaywallProOptions {
+    
+}
